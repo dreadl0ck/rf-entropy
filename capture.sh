c84f245326f2aa7ba7315b3d8d1d3057e60c605f
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-04-15 23:36:13 +0200

    updated capture script

--- capture.sh
+++ capture.sh
@@ -6,19 +6,25 @@
 # 1000000000 = 1 GHz
 # 6000000000 = 6 GHz
 
-sleep=10
+sleep=5
 start=1000000
 end=6000000000
-step=1000000
+step=10000000
 
 echo "current sleep duration = $sleep"
 
 duration=$(((($end-$start)/$step)*$sleep))
 printf 'processing will take %dh:%dm:%ds\n' $((duration/3600)) $((duration%3600/60)) $((duration%60))
 
+res=$((($end-$start)/$step))
+
+echo "and create $res files"
+
+mkdir -p samples
+
 for (( COUNTER=$start; COUNTER<=$end; COUNTER+=$step )); do
     echo "capturing frequency $COUNTER"
-    hackrf_transfer -r data-$COUNTER -f $COUNTER &
+    hackrf_transfer -r samples/data-$COUNTER.wav -f $COUNTER &
 
     sleep $sleep
 
