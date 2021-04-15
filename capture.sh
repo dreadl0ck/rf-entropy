8807c2cb1272036ee042e00d0b1ca991e44d1552
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-04-15 10:20:04 +0200

    added capture script

---
+++ capture.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script to capture samples from a configurable frequency band using the HackRF.
+
+# 1000000 = 1 MHz
+# 1000000000 = 1 GHz
+# 6000000000 = 6 GHz
+
+sleep=10
+start=1000000
+end=6000000000
+step=1000000
+
+echo "current sleep duration = $sleep"
+
+duration=$(((($end-$start)/$step)*$sleep))
+printf 'processing will take %dh:%dm:%ds\n' $((duration/3600)) $((duration%3600/60)) $((duration%60))
+
+for (( COUNTER=$start; COUNTER<=$end; COUNTER+=$step )); do
+    echo "capturing frequency $COUNTER"
+    hackrf_transfer -r data-$COUNTER -f $COUNTER &
+
+    sleep $sleep
+
+    # kill via signal after sleep duration
+    kill -INT $(pgrep hackrf_transfer)
+done
