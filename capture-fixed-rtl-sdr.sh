0e0d750363806e0262ede62ea8f0c3f0c5929629
Philipp Mieden <dreadl0ck@protonmail.ch>
2021-04-16 10:58:10 +0200

    updated scripts

---
+++ capture-fixed-rtl-sdr.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Script to capture samples from a configurable frequency band using the HackRF.
+
+# 1000000 = 1 MHz
+# 1000000000 = 1 GHz
+# 6000000000 = 6 GHz
+
+# - 95000000: 95 MHz (FM)
+# - 145800000: 145.80 MHz (ISS)
+# - 433000000: 433 MHz (Amateur)
+# - 790000000: 790 MHz (MFCN, PPDR)
+# - 862000000: 862 MHz
+# - 1300000000: 1300 MHz (Aeronautical)
+# - 1559000000: 1559 MHz (GNSS: Glonass, Galileo)
+# - 2200000000: 2200 MHz (Space research, Radio Astronomy)
+
+declare -a arr=("95000000"
+        "145800000"
+        "433000000"
+        "790000000"
+        "862000000"
+        "1300000000"
+        "1559000000"
+        "2200000000"
+        )
+
+mkdir -p samples-rtl
+
+for f in "${arr[@]}"; do
+    echo "capturing frequency $f"
+    rtl_sdr -f $f -s 2500000 -n 500000000 "samples-rtl/data-$f.wav"
+done
